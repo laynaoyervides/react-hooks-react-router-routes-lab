e7d9f4326afd59c69aff2b7119823d904d058502
Layna Oyervides <laynaoyervides@gmail.com>
2022-01-14 10:27:50 -0700

    Completed Assginment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,27 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieIems = movies.map((movie)=>(
+      <div key= {movie.title}>
+        <h2>{movie.title}</h2>
+        <p>Runtime: {movie.time} min.</p>
+        <ul>
+          {movie.genres.map( (genre) =>
+                (<li key={genre}>{genre}</li>
+            ))
+            };
+        </ul>
+      </div>
+
+  ))
+  return (
+  <div>
+    <h1>
+      Movies Page
+    </h1>
+    {movieIems}
+  </div>
+  );
 }
 
 export default Movies;
